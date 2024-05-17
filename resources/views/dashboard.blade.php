7313ef1d9979a5c162636c5ba26babbb70042401
mhusef <mhusef71@gmail.com>
2024-05-17 04:31:27 +0300

    gg2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -348,43 +348,57 @@ var options = {
         chart.render();
 
 
-
         var options = {
-          series: [25, 15, 44, 55, 41, 17],
-          chart: {
-          width: '100%',
-          type: 'pie',
-        },
-        labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-        theme: {
-          monochrome: {
-            enabled: true
+          series: [
+          {
+            data: [
+              {
+                x: 'Code',
+                y: [
+                  new Date('2019-03-02').getTime(),
+                  new Date('2019-03-04').getTime()
+                ]
+              },
+              {
+                x: 'Test',
+                y: [
+                  new Date('2019-03-04').getTime(),
+                  new Date('2019-03-08').getTime()
+                ]
+              },
+              {
+                x: 'Validation',
+                y: [
+                  new Date('2019-03-08').getTime(),
+                  new Date('2019-03-12').getTime()
+                ]
+              },
+              {
+                x: 'Deployment',
+                y: [
+                  new Date('2019-03-12').getTime(),
+                  new Date('2019-03-18').getTime()
+                ]
+              }
+            ]
           }
+        ],
+          chart: {
+          height: 350,
+          type: 'rangeBar'
         },
         plotOptions: {
-          pie: {
-            dataLabels: {
-              offset: -5
-            }
+          bar: {
+            horizontal: true
           }
         },
-        title: {
-          text: "Monochrome Pie"
-        },
-        dataLabels: {
-          formatter(val, opts) {
-            const name = opts.w.globals.labels[opts.seriesIndex]
-            return [name, val.toFixed(1) + '%']
-          }
-        },
-        legend: {
-          show: false
+        xaxis: {
+          type: 'datetime'
         }
         };
 
         var chart = new ApexCharts(document.querySelector("#chartjs"), options);
         chart.render();
-
 
     </script>
 </body>
